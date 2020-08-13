3778edf4f506298e93d446ff70ade9a04fa0b75b
luc-dotcom <lucm.dotcom@gmail.com>
2020-08-13 21:45:02 +0200

    *.

--- test/hover_ussd_test.dart
+++ test/hover_ussd_test.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:hover_ussd/hover_ussd.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('hover_ussd');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
 }
